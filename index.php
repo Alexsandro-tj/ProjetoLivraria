2400f64b4e4e689d250639e166db0a15a4e57064
ALEX SANDRO SOUZA DOS SANTOS <alexsandrotj94@gmail.com>
2026-07-30 21:22:31 -0300

    Primeiro commit.

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+class Livros
+{
+    public string $titulo, $autor;
+    public int $ano, $isbn;
+    public bool $disponivel;
+
+
+
+    public function __construct(string $titulo, string $autor, int $ano, int $isbn)
+    {
+        $this->titulo = $titulo;
+        $this->autor = $autor;
+        $this->ano = $ano;
+        $this->isbn = $isbn;
+        $this->disponivel = true;
+    }
+}
+
+
+class Biblioteca /* extends Livros */
+{
+    private array $livros = [];
+
+
+    public function cadastrarLivro(Livros $l)
+    {
+        $this->livros[] = $l;
+        if ($this->livros) {
+
+            echo "Livro cadastrado com sucesso!<br>";
+        } else {
+            echo "Livro Não Cadastrado!";
+        }
+    }
+
+    public function listarLivros()
+    {
+        foreach ($this->livros as $key) {
+            echo "--------------------------<br>";
+            echo "<strong>Titulo</strong>: {$key->titulo}<br>";
+            echo "<strong>Autor</strong>: {$key->autor}<br>";
+            echo "<strong>Ano</strong>: {$key->ano}<br>";
+            echo "<strong>Isbn</strong>: {$key->isbn}<br>";
+            
+        }
+    }
+
+    /*
+buscar por título;
+emprestar livro;
+devolver livro;
+remover livro; */
+}
+$newLivro = new Livros("Bíblia", "Torre de Vigia", "2018", 1000);
+$newLivro2 = new Livros("Seja Feliz", "Torre de Vigia", "2025", 1001);
+
+
+$novoLivro = new Biblioteca();
+$novoLivro->cadastrarLivro($newLivro);
+$novoLivro->cadastrarLivro($newLivro2);
+$novoLivro->listarLivros();
