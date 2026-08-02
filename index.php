b5b5e48ada4d74448f271ab4cc3b45c4c051f8a2
ALEX SANDRO SOUZA DOS SANTOS <alexsandrotj94@gmail.com>
2026-08-02 18:32:25 -0300

    retirada classe Biblioteca

--- index.php
+++ index.php
@@ -1,41 +1,9 @@
 <?php
-include_once"Livros.php";
-
-class Biblioteca 
-{
-    public array $livros = [];
+include_once "Livros.php";
+include_once"Biblioteca.php";
 
 
-    public function cadastrarLivro(Livros $l)
-    {
-        $this->livros[] = $l;
-        if ($this->livros) {
-
-            echo "Livro cadastrado com sucesso!<br>";
-        } else {
-            echo "Livro Não Cadastrado!";
-        }
-    }
-
-    public function getLivros()
-    {
-        foreach ($this->livros as $key) {
-            echo "--------------------------<br>";
-            echo "<strong>Titulo</strong>: {$key->titulo}<br>";
-            echo "<strong>Autor</strong>: {$key->autor}<br>";
-            echo "<strong>Ano</strong>: {$key->ano}<br>";
-            echo "<strong>Isbn</strong>: {$key->isbn}<br>";
-            
-        }
-    }
-
-    /*
-buscar por título;
-emprestar livro;
-devolver livro;
-remover livro; */
-}
-$newLivro = new Livros("Bíblia", "Torre de Vigia", "2018", 1000);
+$newLivro = new Livros("Biblia", "Torre de Vigia", "2018", 1000);
 $newLivro2 = new Livros("Seja Feliz", "Torre de Vigia", "2025", 1001);
 
 
@@ -43,3 +11,9 @@ $novoLivro = new Biblioteca();
 $novoLivro->cadastrarLivro($newLivro);
 $novoLivro->cadastrarLivro($newLivro2);
 $novoLivro->getLivros();
+$resultado = $novoLivro->buscarPorTitulo("Seja Feliz");
+if ($resultado) {
+    echo "Livro encontrado: " . $resultado->titulo;
+} else {
+    echo "Livro não encontrado";
+}
